158a391fbe62e0faa05b0301534d34b10394f05d
anr41 <anr41@case.edu>
2018-04-16 08:42:11 -0400

    Update urls.py

--- Web/lampisite/lampi/urls.py
+++ Web/lampisite/lampi/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, re_path
-
+from django.views.decorators.cache import cache_page
 from . import views
 
 app_name = 'lampi'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('add/', views.AddLampiView.as_view(), name='add'),
+    path('', cache_page(60*15)(views.IndexView.as_view()), name='index'),
+    path('add/', cache_page(60*15)(views.AddLampiView.as_view()), name='add'),
     re_path(r'^device/(?P<device_id>[0-9a-fA-F]+)$',
-            views.DetailView.as_view(), name='detail'),
+            cache_page(60*15)(views.DetailView.as_view()), name='detail'),
     path('dashboard', views.DashboardView.as_view(), name='dashboard'),
 ]
